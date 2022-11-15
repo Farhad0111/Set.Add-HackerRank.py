5f7dc82abb9bf62076e9d89464dc384acac352e8
Farhad0111 <109299643+Farhad0111@users.noreply.github.com>
2022-11-15 20:51:19 +0600

    Add files via upload

---
+++ Set .add().py
@@ -0,0 +1,9 @@
+class Farhad:
+    def __init__(self,a):
+        self.a=a
+    def Faru(self):
+        print(len(self.a))
+i=int(input())
+farhad=Farhad(a=set(input() for j in range(i)))
+farhad.Faru()
+
